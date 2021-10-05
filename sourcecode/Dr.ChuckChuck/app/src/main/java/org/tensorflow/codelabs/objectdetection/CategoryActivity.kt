728448f664a0ece6b5e741292fe7448d44bcd464
Taeyoon Kwon <tykwon97@naver.com>
2021-10-05 16:07:49 +0900

    layout(intro, 자세한 분리수거)기본 틀 제작

---
+++ sourcecode/Dr.ChuckChuck/app/src/main/java/org/tensorflow/codelabs/objectdetection/CategoryActivity.kt
@@ -0,0 +1,14 @@
+package org.tensorflow.codelabs.objectdetection
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.tensorflow.odelabs.objectdetection.R
+
+class CategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+    }
+
+
+}
---
+++ sourcecode/Dr.ChuckChuck/app/src/main/java/org/tensorflow/codelabs/objectdetection/IntroActivity.kt
@@ -0,0 +1,30 @@
+package org.tensorflow.codelabs.objectdetection
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import org.tensorflow.odelabs.objectdetection.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+        init()
+    }
+    private fun init(){
+        val btn1 = findViewById<Button>(R.id.start_btn)
+        val btn2 = findViewById<Button>(R.id.category_btn)
+
+
+        btn1.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn2.setOnClickListener {
+            val intent = Intent(this,CategoryActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
